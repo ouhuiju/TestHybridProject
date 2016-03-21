42c3c06a62a4d024fa1edeb0689855ce73555a69
OKAR OU <okar.ou@oocl.com>
2016-03-21 13:49:14 +0800

    init

---
+++ TestHybridProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestHybridProject
+//
+//  Created by OKAR OU on 16/3/21.
+//  Copyright © 2016年 OKAR OU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestHybridProject/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestHybridProject
+//
+//  Created by OKAR OU on 16/3/21.
+//  Copyright © 2016年 OKAR OU. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
